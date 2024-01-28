3d32680db79ee32180113869c166798790bb0ac1
nocis <nocis@tju.edu.cn>
2024-01-27 17:41:37 -0800

    add load script

---
+++ plugin/skeleton.lua
@@ -0,0 +1,13 @@
+vim.api.nvim_create_user_command("TemplateInit", function(opts)
+	local bufname = vim.api.nvim_buf_get_name(0)
+	local filetype = vim.filetype.match({ filename = bufname })
+
+	vim.notify("not empty file" .. bufname .. filetype, vim.log.levels.INFO, {})
+	require("skeleton").runOnCurrentBuffer(filetype)
+end, {})
+
+vim.api.nvim_create_user_command("TemplateEdit", function(opts)
+	local bufname = vim.api.nvim_buf_get_name(0)
+	local filetype = vim.filetype.match({ filename = bufname })
+	require("skeleton").templateEdit(filetype)
+end, {})
