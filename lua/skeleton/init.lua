581549ab33765a714c994a6a7a1844f28421099e
nocis <nocis@tju.edu.cn>
2024-01-27 17:21:13 -0800

    add skeleton

--- lua/skeleton/init.lua
+++ lua/skeleton/init.lua
@@ -5,13 +5,13 @@ function M.setup(options)
 		local filetype = vim.filetype.match({ filename = bufname })
 
 		vim.notify("not empty file" .. bufname .. filetype, vim.log.levels.INFO, {})
-		require("plugins.skeleton-nvim.skeletom-nvim").runOnCurrentBuffer(filetype)
+		require("skeleton").runOnCurrentBuffer(filetype)
 	end, {})
 
 	vim.api.nvim_create_user_command("TemplateEdit", function(opts)
 		local bufname = vim.api.nvim_buf_get_name(0)
 		local filetype = vim.filetype.match({ filename = bufname })
-		require("plugins.skeleton-nvim.skeletom-nvim").templateEdit(filetype)
+		require("skeleton").templateEdit(filetype)
 	end, {})
 end
 return M
