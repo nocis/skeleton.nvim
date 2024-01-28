6ecd7cc424a286b592c213656a32dd72bc658ac2
nocis <nocis@tju.edu.cn>
2024-01-27 17:43:22 -0800

    test

--- plugin/skeleton.lua
+++ plugin/skeleton.lua
@@ -2,7 +2,7 @@ vim.api.nvim_create_user_command("TemplateInit", function(opts)
 	local bufname = vim.api.nvim_buf_get_name(0)
 	local filetype = vim.filetype.match({ filename = bufname })
 
-	vim.notify("not empty file" .. bufname .. filetype, vim.log.levels.INFO, {})
+	vim.notify("not empty file" .. bufname, vim.log.levels.INFO, {})
 	require("skeleton").runOnCurrentBuffer(filetype)
 end, {})
 
