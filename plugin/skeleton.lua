63229b127bcca46b0e62df0a93644e5c28a45cbf
Jared Yang <nocis@tju.edu.cn>
2024-02-11 17:21:35 -0800

    Update skeleton.lua

--- plugin/skeleton.lua
+++ plugin/skeleton.lua
@@ -1,19 +1,25 @@
 vim.api.nvim_create_user_command("TemplateInit", function(opts)
-	-- local filetype = vim.filetype.match({ buf = 0 })
+	local filetype = vim.filetype.match({ buf = 0 })
         local filename = vim.api.nvim_buf_get_name(0)
 	local extension = vim.fn.fnamemodify(filename, ":e")
 	if extension == nil or extension == "" then
 	    extension = vim.fn.fnamemodify(filename, ":t:r")
 	end
+	if filetype == 'cmake' then
+	    extension = filetype
+	end
 	require("skeleton").runOnCurrentBuffer(extension)
 end, {})
 
 vim.api.nvim_create_user_command("TemplateEdit", function(opts)
-	-- local filetype = vim.filetype.match({ buf = 0 })
+	local filetype = vim.filetype.match({ buf = 0 })
 	local filename = vim.api.nvim_buf_get_name(0)
 	local extension = vim.fn.fnamemodify(filename, ":e")
 	if extension == nil or extension == "" then
 	    extension = vim.fn.fnamemodify(filename, ":t:r")
+	end
+	if filetype == 'cmake' then
+	    extension = filetype
 	end
 	require("skeleton").templateEdit(extension)
 end, {})
